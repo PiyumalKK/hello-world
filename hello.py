f1ee5de04472b24c0d8c0f911c66324ca4352af9
Piyumal Ranasinghe <142605985+PiyumalKK@users.noreply.github.com>
2025-01-18 15:13:25 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                script {
+                    // Python code to print 'Hello World'
+                    sh 'python3 -c "print(\'Hello World\')"'
+                }
+            }
+        }
+    }
+}
 
